9a7e7c12db19c135d268ae58a06c6205b3bd5826
thierry habart <habarthierry@hotmail.fr>
2019-04-04 11:21:30 +0200

    Update UT

--- SimpleIdServer/tests/SimpleIdServer.IdentityStore.LDAP.Tests/LdapHelperFixture.cs
+++ SimpleIdServer/tests/SimpleIdServer.IdentityStore.LDAP.Tests/LdapHelperFixture.cs
@@ -21,17 +21,31 @@ namespace SimpleIdServer.IdentityStore.LDAP.Tests
         }
 
         [Fact]
-        public void When_GetPagedResult_Then_List_Is_Returned()
+        public void When_Authenticate_With_Admin_Then_True_Is_Returned()
         {
             // ARRANGE
             InitializeFakeObjects();
 
             // ACT
-            var result = _ldapHelper.GetPagedUsers(10, 0);
+            var ldapConnection = _ldapHelper.AuthenticateAsAdmin();
 
             // ASSERT
-            Assert.NotNull(result);
-            Assert.Equal(10, result.Count);
+            Assert.NotNull(ldapConnection);
+            ldapConnection.Dispose();
+        }
+
+        [Fact]
+        public void When_Authenticate_With_LoginPassword_Then_True_Is_Returned()
+        {
+            // ARRANGE
+            InitializeFakeObjects();
+
+            // ACT
+            var ldapConnection = _ldapHelper.AuthenticateBasicLoginAndPassword("cn=thabart,cn=users,cn=system", "password");
+
+            // ASSERT
+            Assert.NotNull(ldapConnection);
+            ldapConnection.Dispose();
         }
 
         [Fact]
@@ -48,13 +62,13 @@ namespace SimpleIdServer.IdentityStore.LDAP.Tests
         }
 
         [Fact]
-        public void When_GetUser_Then_One_Record_Is_Returned()
+        public void When_GetAllCredentialSettings_Then_List_Is_Returned()
         {
             // ARRANGE
             InitializeFakeObjects();
 
             // ACT
-            var result = _ldapHelper.GetUser("Habart Thierry");
+            var result = _ldapHelper.GetAllCredentialSettings();
 
             // ASSERT
             Assert.NotNull(result);
@@ -69,13 +83,69 @@ namespace SimpleIdServer.IdentityStore.LDAP.Tests
             // ACT
             var result = _ldapHelper.GetUsers(new[]
             {
-                "Habart Thierry"
+                "thabart",
+                "laetitia"
             });
 
             // ASSERT
             Assert.NotNull(result);
+            Assert.Equal(2, result.Count);
         }
-        
+
+        [Fact]
+        public void When_GetPagedResult_Then_List_Is_Returned()
+        {
+            // ARRANGE
+            InitializeFakeObjects();
+
+            // ACT
+            var firstUser = _ldapHelper.GetPagedUsers(1, 0);
+            var secondUser = _ldapHelper.GetPagedUsers(1, 1);
+
+            // ASSERT
+            Assert.NotNull(firstUser);
+            Assert.NotNull(secondUser);
+            Assert.Equal(1, firstUser.Count);
+            Assert.Equal(1, secondUser.Count);
+        }
+
+        [Fact]
+        public void When_GetUser_Then_One_Record_Is_Returned()
+        {
+            // ARRANGE
+            InitializeFakeObjects();
+
+            // ACT
+            var result = _ldapHelper.GetUser("thabart");
+
+            // ASSERT
+            Assert.NotNull(result);
+        }
+
+        [Fact]
+        public void When_GetUserCredential_Then_One_Record_Is_Returned()
+        {
+            // TODO
+        }
+
+        [Fact]
+        public void When_GetCredentialSetting_Then_One_Record_Is_Returned()
+        {
+            // TODO
+        }
+
+        [Fact]
+        public void When_DeleteUser_Then_Ok_Is_Returned()
+        {
+            // TODO
+        }
+
+        [Fact]
+        public void When_DeleteUserCredential_Then_Ok_Is_Returned()
+        {
+            // TODO
+        }
+
         private void InitializeFakeObjects()
         {
             _options = new IdentityStoreLDAPOptions
@@ -84,12 +154,15 @@ namespace SimpleIdServer.IdentityStore.LDAP.Tests
                 {
                     Server = "127.0.0.1",
                     Port = 389,
+                    // Port = 10389,
                     LDAPFilterAllUsers = "(objectClass=person)",
+                    LDAPFilterAllCredentialSettings = "(objectClass=credentialSetting)",
                     LDAPUserAttributeIdentifier = "cn"
                 },
                 Authenticate = new LDAPAuthenticateOptions
                 {
                     UserName = "cn=thabart,cn=users,cn=system",
+                    // UserName = "uid=admin,ou=system",
                     Password = "password",
                     AuthType = System.DirectoryServices.Protocols.AuthType.Basic,
                     Version = 3
